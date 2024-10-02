4a2b56f7fe570423132923c759c31b0a6542837f
Jiteshiyu <109058809+Jiteshiyu@users.noreply.github.com>
2024-10-02 17:52:51 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,10 @@ import { GlobalContext } from "../context/index.jsx";
 import RecipeItem from "../components/RecipeItem.jsx";
 
 export default function Home() {
+  // Access recipe list and loading state from context
   const { recipeList, loading } = useContext(GlobalContext);
 
+  // Show loading message while data is being fetched
   if (loading)
     return (
       <div className="text-2xl text-center text-black font-extrabold my-6">
@@ -14,9 +16,11 @@ export default function Home() {
 
   return (
     <div className="pt-4 pb-12 container mx-auto flex flex-wrap justify-center gap-10">
+      {/* Map through the recipe list and display each recipe item */}
       {recipeList && recipeList.length > 0 ? (
         recipeList.map((item) => <RecipeItem key={item.id} item={item} />)
       ) : (
+        // Display a message if the recipe list is empty
         <div>
           <p className="lg:text-4xl text-xl text-center text-black font-extrabold">
             Please search something!
