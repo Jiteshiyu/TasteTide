83de6c25ee4b525342dfb2169e5c02eb2682a026
Jiteshiyu <109058809+Jiteshiyu@users.noreply.github.com>
2024-10-02 17:52:02 +0530

    Update Favorites.jsx

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -3,13 +3,16 @@ import RecipeItem from "../components/RecipeItem.jsx";
 import { GlobalContext } from "../context/index.jsx";
 
 export default function Favorites() {
+  // Access the favorites list from context
   const { favoritesList } = useContext(GlobalContext);
 
   return (
     <div className="pt-4 pb-12 container mx-auto flex flex-wrap justify-center gap-10">
+      {/* Map through the favorites list and display each recipe item */}
       {favoritesList && favoritesList.length > 0 ? (
         favoritesList.map((item) => <RecipeItem key={item.id} item={item} />)
       ) : (
+        // Display a message if there are no favorite recipes
         <div>
           <p className="lg:text-4xl text-xl text-center text-black font-extrabold">
             Nothing is added in favorites.
