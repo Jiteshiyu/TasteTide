fd3a80bdbf081e50539401a63af63df1872727c2
Jiteshiyu <109058809+Jiteshiyu@users.noreply.github.com>
2024-10-02 17:48:04 +0530

    Update RecipeItem.jsx

--- src/components/RecipeItem.jsx
+++ src/components/RecipeItem.jsx
@@ -3,16 +3,22 @@ import { Link } from "react-router-dom";
 export default function RecipeItem({ item }) {
   return (
     <div className="flex flex-col w-80 overflow-hidden p-5 bg-white/75 shadow-xl gap-5 border-2 rounded-2xl border-white">
+      {/* Image container */}
       <div className="h-40 flex justify-center overflow-hidden items-center rounded-xl">
         <img src={item?.image_url} alt="recipe item" className="block w-full" />
       </div>
+
+      {/* Recipe details */}
       <div>
+        {/* Publisher of the recipe */}
         <span className="text-base text-red-500 font-medium">
           {item?.publisher}
         </span>
+        {/* Title of the recipe */}
         <h3 className="font-bold text-3xl truncate text-black">
           {item?.title}
         </h3>
+        {/* Link to recipe details */}
         <Link
           to={`/recipe-item/${item?.id}`}
           className="text-base p-3 mt-5 px-8 rounded-lg uppercase font-medium tracking-wider inline-block shadow-md bg-blue-500 text-white"
