ebeb2866f3e9c235b7098f5f98453bb645b3931d
Jiteshiyu <109058809+Jiteshiyu@users.noreply.github.com>
2024-10-02 17:46:34 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,25 +4,30 @@ import { GlobalContext } from "../context/index.jsx";
 
 export default function Navbar() {
   const { searchParam, setSearchParam, handleSubmit } =
-    useContext(GlobalContext);
+    useContext(GlobalContext); // Access global context values
 
   return (
     <nav className="flex justify-between items-center pb-4 container mx-auto flex-col lg:flex-row gap-5 lg:gap-0">
+      {/* Logo with link to home */}
       <h2 className="text-3xl text-red-600 font-bold">
         <NavLink to={"/"}>TasteTide</NavLink>
       </h2>
+      {/* Search form */}
       <form onSubmit={handleSubmit}>
         <input
           type="text"
           name="search"
           value={searchParam}
+          // Update search param
           onChange={(event) => setSearchParam(event.target.value)}
           placeholder="Enter Items..."
           className="bg-white/75 p-3 px-8 rounded-full outline-none lg:w-96 shadow-lg shadow-red-100 focus:shadow-red-200"
         />
       </form>
+      {/* Navigation links */}
       <ul className="flex gap-5">
         <li>
+          {/* Home page link */}
           <NavLink
             to={"/"}
             className="text-black text-xl font-medium hover:text-gray-700 duration-300"
@@ -31,11 +36,12 @@ export default function Navbar() {
           </NavLink>
         </li>
         <li>
+          {/* Favorites link */}
           <NavLink
             to={"/favorites"}
             className="text-black text-xl font-medium hover:text-gray-700 duration-300"
           >
-            favorites
+            Favorites
           </NavLink>
         </li>
       </ul>
